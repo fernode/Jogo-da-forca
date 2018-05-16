0f1ff7dc9f40c21b602b07737b79c50c96938696
fernode <fernandosilvadj@hotmail.com>
2018-05-16 03:25:34 -0300

    Implementando função que lê as palavras no dicionário

--- forca.rb
+++ forca.rb
@@ -1,5 +1,15 @@
 require_relative "ui.rb"
 
+def escolhe_palavra_secreta
+    avisa_escolhendo_palavra
+    texto = File.read("dicionario.txt")
+    todas_as_palavras = texto.split("\n")
+    numero_escolhido = rand(todas_as_palavras.size)
+    palavra_secreta = todas_as_palavras[numero_escolhido]
+    avisa_palavra_escolhida palavra_secreta
+    return palavra_secreta
+end
+
 def palavra_mascarada(chutes, palavra_secreta)
     mascara = ""
     for letra in palavra_secreta.chars
